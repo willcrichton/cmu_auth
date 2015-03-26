935a332548898836f0b70ffeaeacfc2230cbfb4e
Jacob Zimmerman <zimmerman.jake@gmail.com>
2015-03-25 23:25:21 -0400

    Add setup.py
    This file acts as a sort of manifest for declaring things about the
    package.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='cmu_auth',
+      version='0.1.5',
+      description='Simple python methods for authenticating to Carnegie Mellon Unversity services.',
+      url='https://github.com/willcrichton/cmu_auth',
+      author='Will Crichton',
+      author_email='willcrichton@cmu.edu',
+      license='MIT',
+      packages=['cmu_auth'],
+      install_requires=[
+          'requests',
+      ],
+      zip_safe=False)
